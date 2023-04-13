70d4209f36c9d45ddb49209c314be348a0b4436d
code-hyker <shivani2001sk@gmail.com>
2023-04-13 20:35:03 +0530

    First Commit

---
+++ Major Project/src/components/cloud/Cloud.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './CloudStyles.css'
+
+const Cloud = () => {
+    return (
+        <div className='cloud'>
+            <div className="container">
+                <div className="content">
+                    <h2><span>Cloud</span> Security</h2>
+                    <p>Cloud computing security or, more simply, cloud security refers to a broad set of policies, technologies, applications, and controls utilized to protect virtualized IP, data, applications, services, and the associated infrastructure of cloud computing. It is a sub-domain of computer security, network security, and, more broadly, information security.</p>
+                <div><button>Know More</button></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cloud
---
+++ Major Project/src/components/data/Data.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './DataStyles.css'
+
+const Data = () => {
+    return (
+        <div className='data'>
+            <div className="container">
+                <div className="content">
+                    <h2><span>Data</span> Recovery</h2>
+                    <p>In computing, data recovery is a process of salvaging deleted, inaccessible, lost, corrupted, damaged or formatted data from secondary storage, removable media or files, when the data stored in them cannot be accessed in a usual way. The data is most often salvaged from storage media such as internal or external hard disk drives (HDDs), solid-state drives (SSDs), USB flash drives, magnetic tapes, CDs, DVDs, RAID subsystems, and other electronic devices. Recovery may be required due to physical damage to the storage devices or logical damage to the file system that prevents it from being mounted by the host operating system (OS).</p>
+                    <div>
+                        <button>Know More</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Data
